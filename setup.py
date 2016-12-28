3f2f59819fdf5a88c0e339b758634a890dad21d6
hellerve <veit.heller@student.htw-berlin.de>
2016-12-28 20:04:25 +0100

    updated description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1.0',
     license='MIT',
     url='https://github.com/port-zero/lwt',
-    description='Extensible data structure traversal in the command line',
+    description='Get processes with open sockets but no permission to use them',
     download_url = 'https://github.com/port-zero/lwt/tarball/0.1.0',
     packages=find_packages('.'),
     entry_points={
